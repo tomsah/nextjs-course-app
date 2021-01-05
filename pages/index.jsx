faf9247992af83981c94fab0c1b7b2a613cfd899
Thomas Salah <thomas.salah@gmail.com>
2021-01-05 18:45:49 +0000

    Feature: using next Link component for navigation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
 
-
-const Page = () => <div>Index Page</div>
+const Page = () => (<div>
+    <h1>Index Page</h1>
+    <Link href={"/notes"}>
+        Notes
+    </Link>
+</div>)
 
 export default Page
