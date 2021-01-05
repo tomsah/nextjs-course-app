015018c85a75569ee288b65a6927db62be4f7a93
Thomas Salah <thomas.salah@gmail.com>
2021-01-05 17:29:13 +0000

    Feature: creating pages

---
+++ pages/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+const Page = () => <div>Index Page</div>
+
+export default Page
---
+++ pages/notes/[...params].jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useRouter } from "next/router"
+
+const Page = () => {
+    const router = useRouter()
+    const { params } = router.query
+    return (
+        <h1>
+            Note { id }
+        </h1>
+    )
+}
+
+export default Page
