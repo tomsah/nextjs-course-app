ea5c6406c4ed99bd22b2c7347469fe8136d03560
Thomas Salah <thomas.salah@gmail.com>
2021-01-05 20:21:27 +0000

    Styling: using theme-ui

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+/** @jsxRuntime classic /
+ /* @jsx jsx */
+import { jsx } from 'theme-ui'
 import Link from 'next/link'
-import styles from '../src/components/styles.module.css'
 
-const Page = () => (<div className={styles.foo}>
-    <h1>Index Page</h1>
-    <Link href={"/notes"}>
-        Notes
-    </Link>
-</div>)
+const Page = () => (
+    <div sx={{ height: `calc(100vh - 60px)`}}>
+        <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
+            <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+        </div>
+    </div>
+)
 
 export default Page
